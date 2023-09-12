2076433243eefb876d0b2737b1608a914620620d
Matthew <matt@souldo.net>
2023-09-12 12:53:08 -0500

    removed puppet support < 7

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -25,15 +25,10 @@ class bind::defaults (
     $chroot_class           = undef,
     $chroot_dir             = undef,
 ) {
-    # If puppet_version is 7 or greater, use ruby 4 api
-    if $facts['puppetversion'] =~ '[7-8]\.(0|[1-9]\d*)\.(0|[1-9]\d*)' {
-        unless is_bool($supported) {
-    } else {
-        unless $supported.is_a(Boolean) {
-            fail('Please ensure that the dependencies of the bind module are installed and working correctly')
-        }
+    unless is_bool($supported) {
+        fail('Please ensure that the dependencies of the bind module are installed and working correctly')
     }
-    }
+
     unless $supported {
         fail('Platform is not supported')
     }
